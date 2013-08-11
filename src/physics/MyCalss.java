7a15c3e9c74326f0dbfae3b6c5f03b9e3ed46b6b
Yamp <yampolskiydv@gmail.com>
2013-08-11 15:32:10 +0400

    asd

---
+++ src/physics/MyCalss.java
@@ -0,0 +1,11 @@
+package physics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dimitrius
+ * Date: 11.08.13
+ * Time: 15:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyCalss {
+}
